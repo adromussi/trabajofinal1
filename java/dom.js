e0114c299a5e82bc65449d17f8bf283b12a58189
Adromussi <adromussi@gmail.com>
2021-11-22 17:57:21 -0300

    jquery si señor

--- java/dom.js
+++ java/dom.js
@@ -1,4 +1,3 @@
-
 const productos = [
   {
     id: 1,
@@ -42,7 +41,7 @@ const productos = [
     precio: 1800,
     imagen: "../imagenes/bx2.jpg",
   },
-  
+
   {
     id: 8,
     nombre: "COMBO GOLD 30 PIEZAS",
@@ -68,7 +67,13 @@ const productos = [
     imagen: "../imagenes/bx2.jpg",
   },
 ];
-console.log(productos)
+const nuevosProductos = [
+  {  id: 12,  nombre: "COMBO SAHARA 30 PIEZAS", precio: 2700,imagen: "../imagenes/bx2.jpg", },
+  {  id: 13,  nombre: "COMBO CALIENTES 12 PIEZAS" , precio: 1700,imagen: "../imagenes/bx2.jpg",},
+  {  id: 14,  nombre: "PORCION CLASICOS 5 UNIDADES" , precio: 580,imagen: "../imagenes/bx2.jpg",}
+];
+
+console.log(productos);
 
 const nuevoPedido = () => {
   let contenedor = document.getElementById("container");
@@ -86,32 +91,25 @@ const nuevoPedido = () => {
         <a href="#" id='btn${producto.id}' class="btnn btn-primary">LO QUIERO!!</a>
       </div>
     </div>`;
-  card.innerHTML = html;
-  contenedor.appendChild(card);
+    card.innerHTML = html;
+    contenedor.appendChild(card);
 
-  $(`#btn${producto.id}`).on('click', function () {
-    console.log(`YA CARGAMOS ${producto.nombre}`);
-});
-    
+    $(`#btn${producto.id}`).on("click", function () {
+      console.log(`YA CARGAMOS ${producto.nombre}`);
+    });
   });
+  
+  // Se agregan mas productos utilzando jquery
+  for (const producto of nuevosProductos) {
+    $("#container").append(
+      `<div class="card col-sm-12 col-lg-3">
+        <img src="${producto.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <p class="card-text">${producto.precio}</p>
+          <a href="#" id='btn${producto.id}' class="btnn btn-primary">LO QUIERO!!</a>
+        </div>
+      </div>`
+    );
+  }
 };
-
-const nuevosProductos = [
-{  id: 12,  nombre: "COMBO SAHARA 30 PIEZAS", precio: 2700,imagen: "../imagenes/bx2.jpg", },
-{  id: 13,  nombre: "COMBO CALIENTES 12 PIEZAS" , precio: 1700,imagen: "../imagenes/bx2.jpg",},
-{  id: 14,  nombre: "PORCION CLASICOS 5 UNIDADES" , precio: 580,imagen: "../imagenes/bx2.jpg",}];
-
-for (const producto of nuevosProductos) {
-    $("div.card").append(
-  `<div class="card", "col-sm-12", "col-lg-3">
-      <img src="${producto.imagen}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">${producto.precio}</p>
-        <a href="#" id='btn${producto.id}' class="btnn btn-primary">LO QUIERO!!</a>
-      </div>
-    </div>` 
-    )
-};
-
-
